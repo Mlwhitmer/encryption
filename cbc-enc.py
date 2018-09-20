63a8be37b84b38c9d6ae69f1f4692f72d3e2387c
Smanikonda <susie.manikonda@gmail.com>
2018-09-20 17:06:47 -0400

    fixing errors

--- cbc-enc.py
+++ cbc-enc.py
@@ -23,30 +23,31 @@ if input is None or key is None or output is None:
 iv = bytearray(Random.get_random_bytes(16))
 
 #reading in the key
-in = open(keyfile,"rb")
-fKey = in.read().strip()
-in.close()
+inp = open(key,"r")
+fKey = inp.read().strip()
+inp.close()
 
 #block cipher 
 Fk = AES.new(bytearray.fromhex(fKey),AES.MODE_ECB)
 
 #writing out IV to encrypted file
-out = open(outputfile,"wb+")
+out = open(output,"wb+")
 out.write(iv)
 
 pad_neeeded = False;
 bool = True
 
-in = open(inputfile,"rb")
+inM = open(input,"rb")
 while True:
-	m_block = bytearray(in.read(16))
+	m_block = bytearray(inM.read(16))
 	if len(m_block) != 16:
 		pad_needed = True;
 		pads = 16 - len(m_block)
 	if bool:
 		for i in range(0,16):
 			xor_block = iv[i] ^ m_block[i]
-		c_block = Fk.encrypt(bytearray(xor_block))
+		c_block = Fk.encrypt(xor_block)
+		print(c_block)
 		out.write(c_block)
 		bool = False;
 		prev_m_iv = bytearray(c_block)
