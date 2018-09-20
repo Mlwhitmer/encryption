9bb7cc9829bc7feb026aa85c07f02937a071be07
Smanikonda <susie.manikonda@gmail.com>
2018-09-20 01:54:37 -0400

    added basic functionality

--- cbc-enc.py
+++ cbc-enc.py
@@ -21,13 +21,38 @@ if input is None or key is None or output is None:
 	exit(1)
 
 iv = bytearray(Random.get_random_bytes(16))
-print(iv)
+
+#reading in the key
 in = open(keyfile,"rb")
 fKey = in.read().strip()
 in.close()
 
+#block cipher 
+Fk = AES.new(bytearray.fromhex(fKey),AES.MODE_ECB)
+
+#writing out IV to encrypted file
 out = open(outputfile,"wb+")
 out.write(iv)
 
 
+bool = True
 
+in = open(inputfile,"rb")
+while bool:
+	m_block = bytearray(in.read(16))
+	
+	if bool:
+		for i in range(0,16):
+			xor_block = iv[i] ^ m_block[i]
+		c_block = Fk.encrypt(bytearray(xor_block))
+		out.write(c_block)
+		bool = False;
+		prev_m_iv = bytearray(c_block)
+	else:
+		for i in range(0,16):
+			xor_block = prev_m_iv[i] ^ m_block[i]
+		c_block = Fk.encrypt(bytearray(xor_block))
+		out.write(c_block)
+		prev_m_iv = bytearray(c_block)
+
+		 
